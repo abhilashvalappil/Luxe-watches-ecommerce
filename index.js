417a859583d2af27d4ad5381541b33e6575e78be
abhilashvalappil <abhilashvalappil123@gmail.com>
2025-11-26 21:35:37 +0530

    modified index.js file

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.use(express.static(path.join(__dirname, 'public/adminHomeAssets')));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(session({
-  secret: process.env.SESSION_SECRET,
+  secret: process.env.SESSION_SECRET || 'lskjfsljsdljfk',
   resave: false,
   saveUninitialized: false,
   cookie: { secure: false }  
